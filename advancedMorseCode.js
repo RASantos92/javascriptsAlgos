5d829b37f511bdb486232cf7971d0571b3613578
Robert Santos <rasbms81608@gmail.com>
2020-11-05 14:58:37 -0600

    finially a break through, just need to get the number range for the spaceses tuned in

--- advancedMorseCode.js
+++ advancedMorseCode.js
@@ -1,4 +1,4 @@
-const testBits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
+const testBits = "1111000111111111110000111111111111100011111111110000000000011111111110001111111111000111111111110000000000011111100011111100001111110000111111000000000001111111111111111000011111"
 
 var decodeBits = function (bits) {
     console.log(bits)
@@ -16,12 +16,10 @@ var decodeBits = function (bits) {
         preOutput["s" + bits[i] + i] = counter;
         i += counter - 1;
     }
-
     var sumOf1 = 0;
     var sumOf0 = 0;
     var counter2 = 0;
     var counter3 = 0;
-
     for (var key in preOutput) {
         var value = preOutput[key];
         if (parseInt(key.slice(1, 2)) == 1) {
@@ -33,7 +31,6 @@ var decodeBits = function (bits) {
             sumOf0 += value;
         }
     }
-
     var averageOf1 = sumOf1 / counter2;
     console.log(counter2)
     var averageOf0 = (sumOf0 / counter3) * 2;
@@ -46,7 +43,6 @@ var decodeBits = function (bits) {
                     return output += "-";
                 } 
                 if (value == 1) {
-                    console.log("here")
                     output += "."
                 } 
                 if(value == 3){
@@ -57,7 +53,6 @@ var decodeBits = function (bits) {
                         output += "-"
                     } 
                     else {
-                        console.log("there")
                         output += "."
                     }
                 }
@@ -72,7 +67,8 @@ var decodeBits = function (bits) {
                         console.log(averageOf0)
                         output += "  "
                     }
-                    if (value <= averageOf0 && value >= 3 || value == (sumOf0 / counter3)) {
+                    if (value <= averageOf0 && value >= ((sumOf0 /counter3) + 3) || value == (sumOf0 / counter3)) {
+                        console.log("avg of 0: ", averageOf0, "compair value: ",(sumOf0 /counter3))
                         output += " "
                     }
                 }
@@ -88,61 +84,61 @@ var decodeBits = function (bits) {
 }
 console.log(decodeBits(testBits))
 
-// function decodeMorse(str) {
-//     var morseCodeDict = {
-//         ".-": "A",
-//         "-...": "B",
-//         "-.-.": "C",
-//         "-..": "D",
-//         ".": "E",
-//         "..-.": "F",
-//         "--.": "G",
-//         "....": "H",
-//         "..": "I",
-//         ".---": "J",
-//         "-.-": "K",
-//         ".-..": "L",
-//         "--": "M",
-//         "-.": "N",
-//         "---": "O",
-//         ".--.": "P",
-//         "--.-": "Q",
-//         ".-.": "R",
-//         "...": "S",
-//         "-": "T",
-//         "..-": "U",
-//         "...-": "V",
-//         ".--": "W",
-//         "-..-": "X",
-//         "-.--": "Y",
-//         "--..": "Z",
-//         ".----": "1",
-//         "..---": "2",
-//         "...--": "3",
-//         "....-": "4",
-//         ".....": "5",
-//         "-....": "6",
-//         "--...": "7",
-//         "---..": "8",
-//         "----.": "9",
-//         "-----": "0",
-//         "...---...": "SOS"
-//     }
-//     var words = (str).split("  ");
-//     var letters = words.map((w) => w.split(" "))
-//     var decoded = [];
+function decodeMorse(str) {
+    var morseCodeDict = {
+        ".-": "A",
+        "-...": "B",
+        "-.-.": "C",
+        "-..": "D",
+        ".": "E",
+        "..-.": "F",
+        "--.": "G",
+        "....": "H",
+        "..": "I",
+        ".---": "J",
+        "-.-": "K",
+        ".-..": "L",
+        "--": "M",
+        "-.": "N",
+        "---": "O",
+        ".--.": "P",
+        "--.-": "Q",
+        ".-.": "R",
+        "...": "S",
+        "-": "T",
+        "..-": "U",
+        "...-": "V",
+        ".--": "W",
+        "-..-": "X",
+        "-.--": "Y",
+        "--..": "Z",
+        ".----": "1",
+        "..---": "2",
+        "...--": "3",
+        "....-": "4",
+        ".....": "5",
+        "-....": "6",
+        "--...": "7",
+        "---..": "8",
+        "----.": "9",
+        "-----": "0",
+        "...---...": "SOS"
+    }
+    var words = (str).split("  ");
+    var letters = words.map((w) => w.split(" "))
+    var decoded = [];
+    for (var i = 0; i < letters.length; i++) {
+        decoded[i] = [];
+        for (var j = 0; j < letters[i].length; j++) {
+            if (morseCodeDict[letters[i][j]]) {
+                console.log("here")
+                decoded[i].push(morseCodeDict[letters[i][j]]);
+            }
+        }
+    }
+    return decoded.map(arr => arr.join("")).join(' ');
+}
 
-//     for (var i = 0; i < letters.length; i++) {
-//         decoded[i] = [];
-//         for (var j = 0; j < letters[i].length; j++) {
-//             if (morseCodeDict[letters[i][j]]) {
-//                 decoded[i].push(morseCodeDict[letters[i][j]]);
-//             }
-//         }
-//     }
-//     return decoded.map(arr => arr.join("")).join(' ');
-// }
+var x = decodeBits(testBits);
 
-// var x = decodeBits(testBits);
-
-// console.log(decodeMorse(x))
+console.log(decodeMorse(x))
