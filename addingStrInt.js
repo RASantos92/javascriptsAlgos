6027382af127031b9bc620000742ebaf7730f918
Robert Santos <rasbms81608@gmail.com>
2020-11-10 20:40:45 -0600

    small addition

---
+++ addingStrInt.js
@@ -0,0 +1,10 @@
+const str1 = "11111111111111111111"
+const str2 = "11111111111111111111"
+function addBig(str1, str2){
+    if(str1.length < )
+    var output = parseInt(str1)
+    var output1 = parseInt(str2)
+    var finalOutput = output + output1
+    return finalOutput.toString()
+}
+console.log(addBig("111","222"))
